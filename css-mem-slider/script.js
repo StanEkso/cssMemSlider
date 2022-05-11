ad9eb535c4e9a68e24576de6be628b2d0017573c
StanEkso <90618842+StanEkso@users.noreply.github.com>
2022-05-11 22:12:00 +0300

    fix: change in structure (Wed, May 11,2022 22:16:08)

--- css-mem-slider/script.js
+++ css-mem-slider/script.js
@@ -21,14 +21,16 @@ class Slider {
     #length;
     #data;
     #position;
+    #captions;
     constructor(root, data) {
         this.#node = root;
         this.#items = this.#node.querySelector('.slider__items');
+        this.#captions = this.#node.querySelector('.slider__captions')
         this.#controls = this.#node.querySelector('.slider__controls');
         this.#data = data;
         this.#length = data.length;
         this.#data.forEach(mem => {
-            this.#items.append(this.#createMem(mem))
+            this.#createMem(mem);
         })
         for (let i = 0; i < this.#length; i++) {
             this.#controls.append(this.#createControl(i));
@@ -36,11 +38,14 @@ class Slider {
         this.#setCurrentSlide(0)
     } 
     #setCurrentSlide(number) {
-        if (document.querySelector('.slider__control._active')) document.querySelector('.slider__control._active').classList.remove('_active')
+        const exist = document.querySelector('.slider__control._active');
+        if (exist) exist.classList.remove('_active');
+
         this.#position = number;
-        this.#items.style.transform = `translate(-${100*this.#position}%,0)`;
-        this.#controls.childNodes[number+1].classList.add('_active')
-        
+        console.log(this.#controls.querySelectorAll('.slider__control'))
+        this.#controls.querySelectorAll('.slider__control')[this.#position].classList.add('_active')
+        this.#captions.style.transform = `translate(-${this.#position*100}%,0)`
+        this.#items.style.transform = `translate(-${this.#position*100}%,0)`
     }
     #createControl(index) {
         const control = document.createElement('div');
@@ -51,22 +56,16 @@ class Slider {
         return control
     }
     #createMem(object) {
-        let container = document.createElement('div');
-        container.classList.add('slider-item')
-        let mem = document.createElement('div');
-
-        mem.classList.add('slider-item__image')
-        let img = document.createElement('img');
-        img.src = object.url;
-        mem.append(img);
-
-        container.append(mem);
-
-        const text = document.createElement('div');
-        text.classList.add('slider-item__text');
-        text.textContent = object.name;
-        container.append(text)
-        return container;
+       const imageContainer = document.createElement('div');
+       imageContainer.classList.add('slider-item__image')
+       const img = document.createElement('img');
+       img.src = object.url;
+        imageContainer.append(img);
+        this.#items.append(imageContainer);
+       const caption = document.createElement('div');
+       caption.textContent = object.name;
+       caption.classList.add('slider-item__caption')
+       this.#captions.append(caption);
     }
 }
 const sl = new Slider(root, memes)
